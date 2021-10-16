46a5e374a07c562e28b8dad2d360baf0b0a46d1a
mtbishop <bishopmatthew76@gmail.com>
2021-10-15 21:55:45 -0600

    added routes (needs fixed), models, and dependencies

---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+let express = require('express'),
+  router = express.Router();
+
+let authenticationController = require('../controllers/authController.js');
+let foodController = require('../controllers/foodController');
+/**
+ * (POST Method)
+ */
+// SignUp
+router.post('/signup', authenticationController.signup);
+
+//SignIn
+router.post('/signin', authenticationController.signin);
+// router.post('/food', foodController.newFood);
+module.exports = router;
